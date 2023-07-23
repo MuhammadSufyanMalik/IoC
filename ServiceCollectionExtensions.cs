3cf5a69e49c0000c882c5939f569ab728a206295
Muhammad Sufyan MALIK <muhammad.sufyan@ardgrup.com.tr>
2023-07-23 18:08:23 +0300

    Add project files.

---
+++ ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace IoC
+
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDependencyResolvers(this IServiceCollection services, ICoreModule[] modules)
+        {
+            foreach (ICoreModule coreModule in modules)
+            {
+                coreModule.Load(services);
+            }
+
+            return ServiceTool.Create(services);
+        }
+    }
+}
